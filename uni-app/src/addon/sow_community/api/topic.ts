f996346b765376ec013471ed777e551d5d87a813
hsx <569228509@qq.com>
2026-01-25 21:08:57 +0800

    delete:code 移除暂时没有啥用的代码比如种草社区的前端代码

--- uni-app/src/addon/sow_community/api/topic.ts
+++
@@ -1,8 +0,0 @@
-import request from '@/utils/request'
-
-/**
- * 社区话题列表
- */
-export function getTopicList(params: Record<string, any>) {
-    return request.get(`sow_community/topic/list`, params)
-}
