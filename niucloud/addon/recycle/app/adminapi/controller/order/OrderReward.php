5536cca62f7922d88b9a065334175eda7e8e4ad8
hsx <569228509@qq.com>
2026-03-27 09:46:50 +0800

    refactor: 替换基类并简化成功响应方法
    将控制器基类从 app\adminapi\controller\BaseAdminController 替换为 core\base\BaseAdminController。
    移除 $this->success() 调用，直接使用全局的 success() 辅助函数来返回成功响应，使代码更简洁。

--- niucloud/addon/recycle/app/adminapi/controller/order/OrderReward.php
+++ niucloud/addon/recycle/app/adminapi/controller/order/OrderReward.php
@@ -4,20 +4,20 @@ declare(strict_types=1);
 namespace addon\recycle\app\adminapi\controller\order;
 
 use addon\recycle\app\service\admin\order\OrderRewardService;
-use app\adminapi\controller\BaseAdminController;
+use core\base\BaseAdminController;
 
 class OrderReward extends BaseAdminController
 {
     public function getConfig()
     {
         $data = (new OrderRewardService())->getConfig();
-        return $this->success('', $data);
+        return success($data);
     }
 
     public function setConfig()
     {
         $params = $this->request->post();
         (new OrderRewardService())->setConfig($params);
-        return $this->success();
+        return success();
     }
 }
