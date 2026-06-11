53700ec6b8a916630c6a280716c6ea721ba1874a
hsx <569228509@qq.com>
2026-06-11 16:42:00 +0000

    chore: 移除已废弃的 wj_books 图书插件

--- niucloud/addon/wj_books/app/upgrade/v101/upgrade.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE `{{prefix}}wj_books_config`
-ADD COLUMN `price_adjust_rate` decimal(5, 2) NOT NULL DEFAULT 0.00 COMMENT '回收价调整比例，单位%' AFTER `rejected_book_retrieve_days`;
--- niucloud/addon/wj_books/web/api/hello_world.ts
+++
@@ -1,8 +0,0 @@
-
-/**
- * hello world
- */
-export function getHelloWorld() {
-    return request.get('wj_books/hello_world')
-}
-
