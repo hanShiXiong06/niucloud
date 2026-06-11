7f15f6b01ef3ae56facc324be1423e3e1aaf5240
hsx <569228509@qq.com>
2026-06-11 16:42:32 +0000

    fix(command): 加固队列与常驻进程的类列表合并，过滤非字符串项

--- niucloud/app/command/queue/Queue.php
+++ niucloud/app/command/queue/Queue.php
@@ -91,8 +91,9 @@ class Queue extends Command
         $system_dir = root_path() . 'app' . DIRECTORY_SEPARATOR . 'job';
         $addon_dir = root_path() . 'addon' . DIRECTORY_SEPARATOR;
         if (is_dir($system_dir)) {
+            $app_data = [];
             search_dir($system_dir, $app_data, root_path());
-            $class_list = array_merge($class_list, $app_data);
+            $class_list = array_merge($class_list, array_filter($app_data, 'is_string'));
         }
 
         $addons = ( new CoreAddonService() )->getInstallAddonList();
@@ -100,8 +101,9 @@ class Queue extends Command
 
             $addon_path = $addon_dir . $v[ 'key' ] . DIRECTORY_SEPARATOR . 'app' . DIRECTORY_SEPARATOR . 'job';
             if (is_dir($addon_path)) {
+                $addon_data = [];
                 search_dir($addon_path, $addon_data, root_path());
-                $class_list = array_merge($class_list, $addon_data);
+                $class_list = array_merge($class_list, array_filter($addon_data, 'is_string'));
             }
         }
 
